3478375013a061a1b3da5466b19c08a8e3b5cbd0
Milanpatel2008 <Milanpatel2008>
2021-06-11 22:14:14 +0530

    home

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -185,8 +185,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '**** your Email ****'
-EMAIL_HOST_PASSWORD = '**** Your Password ****'
+EMAIL_HOST_USER = 'pkantibhai91@gmail.com'
+EMAIL_HOST_PASSWORD = 'Milanpatel@123'
 
 
 # paypal Integration
